8fc751d9d7bda404d70dd7bcc5471900fe19fbcd
unknown <futuretrader19@gmail.com>
2020-10-29 21:00:19 -0400

    fifth commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //jshint esversion:6
 require('dotenv').config()
 const express = require('express'),
@@ -6,9 +7,11 @@ const express = require('express'),
   mongoose    = require('mongoose'),
   session     =  require('express-session'),
   passport    =  require('passport'),
-passportLocalMongoose = require('passport-local-mongoose'),  
+passportLocalMongoose = require('passport-local-mongoose'),
+GoogleStrategy = require('passport-google-oauth20').Strategy,
+ findOrCreate = require("mongoose-findorcreate"),
+ FacebookStrategy = require('passport-facebook').Strategy;  
     app = express();
-
 
 
 app.use(express.static('public'));
@@ -30,21 +33,89 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ({
    email : String,
-   password: String
+   password: String,
+   googleId: String,
+   facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// con este metodo podemos serialize o deserialize
+// cualquier user sin importar la estrategia si es por google
+// o facebook o twitter
+passport.serializeUser(function(user, done) {
+   done(null, user.id);
+ });
+ 
+ passport.deserializeUser(function(id, done) {
+   User.findById(id, function(err, user) {
+     done(err, user);
+   });
+ });
+
+passport.use(new GoogleStrategy({
+   clientID: process.env.CLIENT_ID,
+   clientSecret: process.env.CLIENT_SECRET,
+   callbackURL: "http://localhost:3000/auth/google/secrets",
+   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+ },
+ function(accessToken, refreshToken, profile, cb) {
+    console.log(profile + "From google");
+   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+     return cb(err, user);
+   });
+ }
+));
+
+passport.use(new FacebookStrategy({
+   clientID: process.env.FACEBOOK_ID,
+   clientSecret: process.env.FACEBOOK_SECRET,
+   callbackURL: "http://localhost:3000/auth/facebook/secrets"
+ },
+ function(accessToken, refreshToken, profile, cb) {
+    console.log(profile + "FROM FACEBOOK");
+   User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+     return cb(err, user);
+   });
+ }
+));
+
 
 app.get("/", function(req,res){
      res.render('home');
 });
+
+// es la informacion que mandamos a google para que nos
+// authenticate el usuario
+app.get("/auth/google",
+   passport.authenticate('google',  { scope: ['profile'] })
+);
+
+// es la informacion que nos manda google a este Router
+// cuando authentifica al usuario
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets route.
+    res.redirect('/secrets');
+  });
+
+
+ app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+
 
 app.get("/login", function(req,res){
    res.render("login");
